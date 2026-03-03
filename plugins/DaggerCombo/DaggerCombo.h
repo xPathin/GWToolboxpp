d66382a4079dce5ba816b32bc94a7dbbbfc36d7f
Patrick Fischer <git@pathin.me>
2026-03-03 22:56:20 +0800

    feat: hold-to-combo mode, retarget on kill, message suppression for DaggerCombo

--- plugins/DaggerCombo/DaggerCombo.h
+++ plugins/DaggerCombo/DaggerCombo.h
@@ -13,6 +13,8 @@ public:
     void Initialize(ImGuiContext* ctx, ImGuiAllocFns allocator_fns, HMODULE toolbox_dll) override;
     void SignalTerminate() override;
     bool CanTerminate() override;
+    void Update(float delta) override;
+    bool WndProc(UINT msg, WPARAM wParam, LPARAM lParam) override;
     void LoadSettings(const wchar_t*) override;
     void SaveSettings(const wchar_t*) override;
     void DrawSettings() override;
