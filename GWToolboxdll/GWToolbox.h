dac55af18e2d9c1fadfc945eefb020ccc81f353c
Patrick Fischer <git@pathin.me>
2026-03-04 22:17:15 +0800

    chore: remove plugin disclaimers and PvP map disable

--- GWToolboxdll/GWToolbox.h
+++ GWToolboxdll/GWToolbox.h
@@ -24,8 +24,6 @@ public:
         static GWToolbox instance;
         return instance;
     }
-    static bool ShouldDisableToolbox(GW::Constants::MapID = (GW::Constants::MapID)0);
-
     static HMODULE GetDLLModule();
     static DWORD __stdcall MainLoop(LPVOID module) noexcept;
 
@@ -34,8 +32,6 @@ public:
     static void Initialize(LPVOID);
     static void SignalTerminate(bool detach_dll = true);
     static void Update(GW::HookStatus* status = nullptr);
-    static void Enable();
-    static void Disable();
     static bool CanTerminate();
 
     static ToolboxIni* OpenSettingsFile();
